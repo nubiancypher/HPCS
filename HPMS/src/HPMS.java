bec4ec06db3ba79971621c307020f0448f42735f
Jamal Bourne <84990104+nubiancypher@users.noreply.github.com>
2022-04-05 19:01:22 -0400

    Add files via upload

--- HPMS/src/HPMS.java
+++ HPMS/src/HPMS.java
@@ -1,11 +1,56 @@
 
 public class HPMS {
 	
-	public static void main() {
-		System.out.println("Test 5");
-		
+	public static void main(String args[]) {
+		int customerID = 0;
+		switch (args[0]) {
+		case "--billing":
+			if(args[1] != null) {
+				if(args[1]=="--customer-id") {
+					runBilling(customerID);
+				}
+			}
+			runBilling();
+			break;
+		case "--patient-portal":
+			if(args[1] != null) {
+				if(args[1]=="customer-id") {
+					runPP(customerID);
+				}
+			}
+			runPP();
+			break;
+		default:
+			System.out.println("Not a valid option");
+			System.exit(1);
+		}
 		
 		
 	}
+
+	/**
+	 * runs patient portal
+	**/
+	private static void runPP() {
+		// TODO Auto-generated method stub
+		
+	}
 	
+	/**
+	 * runs billing portal
+	**/
+	private static void runBilling() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void runBilling(int customerId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void runPP(int customerId) {
+		// TODO Auto-generated method stub
+		
+	}
 }
