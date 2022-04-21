7dd0dfa9037e0645da66f44e96af959778974e4c
nubiancypher <84990104+nubiancypher@users.noreply.github.com>
2022-04-21 13:03:45 -0400

    Add files via upload

--- HPMS/src/HPMS.java
+++ HPMS/src/HPMS.java
@@ -1,56 +1,84 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HPMS {
 	
-	public static void main(String args[]) {
-		int customerID = 0;
-		switch (args[0]) {
-		case "--billing":
-			if(args[1] != null) {
-				if(args[1]=="--customer-id") {
-					runBilling(customerID);
-				}
+	public static void main(String args[]) throws IOException, InterruptedException {
+		//verify that arguments begin with double hyphen
+		if(args.length == 0) {
+			runHPMS();
+		} else {
+			String argument = args[0];
+			if(!valid(argument)) {
+				System.exit(1);
 			}
-			runBilling();
-			break;
-		case "--patient-portal":
-			if(args[1] != null) {
-				if(args[1]=="customer-id") {
-					runPP(customerID);
-				}
+			switch(argument) {
+			case "--patient-portal":
+				runPP();
+				break;
+			case "--billing":
+				runBilling();
+				break;
 			}
-			runPP();
-			break;
-		default:
-			System.out.println("Not a valid option");
-			System.exit(1);
 		}
 		
 		
 	}
+	
+	private static void runHPMS() throws IOException, InterruptedException {
+		System.out.print("Welcome to the Happypets Patient Management Service, what would you like to do?\n"+
+						   "1. HappyPets Billing\n"+
+						   "2. HappyPets Patient Portal\n"+
+						   "Your choice: ");
+		Scanner scan = new Scanner(System.in);
+		int choice = scan.nextInt();
+		switch(choice) {
+		case 1:
+			runBilling();
+			break;
+		case 2:
+			runPP();
+			break;
+		default:
+			System.out.println("Invalid choice");
+			runHPMS();
+		}
+		
+	}
 
-	/**
-	 * runs patient portal
-	**/
-	private static void runPP() {
-		// TODO Auto-generated method stub
-		
+	public static void runBilling() throws IOException, InterruptedException {
+		Billing component = new Billing();
+	}
+	
+	public static void runPP() {
+		System.out.println("This is the method that will implement the patient portal component, whatever that means.");
+		//use patient-portal.jar to access online portal
 	}
 	
 	/**
-	 * runs billing portal
+	 * Makes sure that argument is valid 
 	**/
-	private static void runBilling() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private static void runBilling(int customerId) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private static void runPP(int customerId) {
-		// TODO Auto-generated method stub
-		
+	public static boolean valid(String argument) {
+		if ((argument.charAt(0)=='-') && (argument.charAt(1) =='-')) {
+			//check the rest of the argument
+			int i = 2;
+			String command = "";
+			while(i<argument.length()) {
+				command+=argument.charAt(i);
+				i++;
+			}
+			switch(command) {
+			case "billing":
+				return true;
+			case "patient-portal":
+				return true;
+			default:
+				//invalid command exception
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
 	}
 }
---
+++ HPMS/src/customer.java
@@ -0,0 +1,23 @@
+//represents a customer of happy pets inc
+public class customer {
+	private int id;
+	public String firstName;
+	public String lastName;
+	public String email;
+	private String token;
+	private String country;
+	private String city;
+	private String state;
+	private int userType;
+	private int userStat;
+	private String createdAt;
+	private String updatedAt;
+	private Pet pet;
+	
+	//lots of parameters
+	public customer(int id, String firstName, String lastName, String email, String token,
+					String country, String city, String state, int userType, int userStat,
+					String createdAt, String updatedAt, Pet pet) {
+		
+	}
+}
