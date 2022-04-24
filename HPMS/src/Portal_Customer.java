cf07ef922d7f22252d96f9aa7545bd07e6cf26c4
Junespidey <68677207+Junespidey@users.noreply.github.com>
2022-04-24 04:12:56 -0400

    Create Portal_Customer.java

---
+++ HPMS/src/Portal_Customer.java
@@ -0,0 +1,46 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+//represents a customer of happy pets inc
+public class Customer_Portal {
+    private int id;
+    public String firstName;
+    public String lastName;
+    public String email;
+    private String token;
+    private String country;
+    private String city;
+    private String state;
+    private int userType;
+    private int userStat;
+    private String createdAt;
+    private String updatedAt;
+    public JSONArray pet;
+
+    public Customer_Portal(JSONObject person) {
+        id = person.getInt("customerId");
+        firstName = person.getString("firstName");
+        lastName = person.getString("lastName");
+        email = person.getString("email");
+        token = person.getString("token");
+        country = person.getString("country");
+        city = person.getString("city");
+        state = person.getString("state");
+        userType = person.getInt("userType");
+        userStat = person.getInt("userStatus");
+        createdAt = person.getString("createdAt");
+        updatedAt = person.getString("updatedAt");
+        pet = person.getJSONArray("pets");
+    }
+
+    //what data should people be able to get?
+    public String toString() {
+        return ("Name: "+ firstName+" "+lastName+"\n"+
+                "Email: "+email+"\n"+
+                "Location: "+city+" "+state+", "+country+"\n"+"\n"
+                +"Pets:" +pet
+                );
+    }
+
+
+}
