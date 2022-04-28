ea012492bd3a917fad1609ff7d8fec0960b2910e
Junespidey <68677207+Junespidey@users.noreply.github.com>
2022-04-28 03:07:36 -0400

    Add files via upload

---
+++ HPMS/src/BillingTestRunner.java
@@ -0,0 +1,25 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class BillingTestRunner {
+    public static void main(String[]args){
+        Result result = JUnitCore.runClasses(BillingTest.class);
+
+        if (result.getFailures().size()>0){
+            System.err.println("A unit test has failed");
+            for(Failure failure:result.getFailures()){
+                System.err.println("\t"+ failure.toString());
+
+            }
+            System.exit(1);
+
+
+        }
+        System.out.println("all tests passed");
+        System.exit(0);
+
+
+    }
+
+}
