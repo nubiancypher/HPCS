bd032d5102c8e2126a8da012e7afecdbc10bb48a
Junespidey <68677207+Junespidey@users.noreply.github.com>
2022-04-28 02:24:35 -0400

    Update PortalTest.java

--- HPMS/src/PortalTest.java
+++ HPMS/src/PortalTest.java
@@ -1,49 +1,84 @@
-import org.json.JSONObject;
+import org.json.*;
 import org.junit.*;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class PortalTest {
-    int dummyId;
 
-    //create new instance of Portal
-    public Portal getPortal () throws IOException, InterruptedException {
-        int id=0;
-        return new Portal(dummyId);
 
-    }
+
+
     @Test
     public void getJSON_is_sucess() throws IOException, InterruptedException {
-
-        HttpRequest request = HttpRequest request = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://happypets-2140724455.us-east-1.elb.amazonaws.com/api/patients"))
                 .build();
-        String JsonReturner = getPortal().getJSON();
-        AssertEquals()
+        URI uri = request.uri();
+        String val = uri.toString();
+        assertTrue(val.equals("http://happypets-2140724455.us-east-1.elb.amazonaws.com/api/patients"));
     }
+
 
     @Test
     public void getCustomerID_test_is_success() throws IOException, InterruptedException {
-        dummyId=4051;
-        Portal portal= this.getPortal();
-        String portal= getPortal().getCustomerID(String responseBody);
+        // creates portal instance
+        Portal tester = new Portal();
+        String testResponse=  tester.getJSON();
+        //testing if the values in getCustomerID are customerIDs
+        ArrayList<Integer>testList = tester.getCustomerID(testResponse);
+
+
+
+
+
+
+
+
 
     }
     @Test
-    public void find_test_is_success(){
+    public void find_test_is_success() throws IOException, InterruptedException {
+        Portal tester= new Portal();
+        ArrayList<JSONObject>mockArray = new ArrayList<JSONObject>();
+        JSONObject mock = new JSONObject();
+        JSONObject mock2 = new JSONObject();
+        JSONObject mock3 = new JSONObject();
+        mock.put ("customerID",1234);
+        mock2.put("customerID",1111);
+        mock3.put("CustomerID",4321);
+        mockArray.add(mock);
+        mockArray.add(mock2);
+        mockArray.add(mock3);
+        assertEquals(tester.find(2222,mockArray),null);
+        assertEquals(tester.find(1234,mockArray),1234);
 
+
+    }
+
+
+
+    @Test
+    public void getPortalInfo_is_sucess() throws IOException, InterruptedException {
+        Portal tester =new Portal();
 
 
     }
     @Test
-    public void getPortalInfo_is_sucess(){
+    public void verify_is_success() throws IOException, InterruptedException {
+        // test if it returns false if fed an ID not in the Json
+        Portal tester= new Portal();
+        int nonOfficialID = 1111;
+        boolean verifyTester= tester.verify(nonOfficialID);
+        assertEquals(verifyTester,false);
+
+
 
 
     }
-
-
-
 
 }
+
